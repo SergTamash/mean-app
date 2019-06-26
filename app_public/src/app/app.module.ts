360adf229c734e3d059d71e7e89fe85a522f0f60
tamashevich <tamashev@gmail.com>
2019-06-26 16:23:08 +0300

    Add auth

--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+// import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// import { AuthModule } from './authentication/auth.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    AppRoutingModule
+    // AppRoutingModule,
+    // AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
